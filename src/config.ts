76be85c80ff3a60c283d5c48b672050d0134770d
davidpalricas <davipalricas22@gmail.com>
2026-03-01 14:34:40 +0000

    Config Update

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export interface SectionData {
 }
 
 export const SECTIONS: SectionData[] = [
-  { id: 'about', label: 'About', modelName: 'about', position: [-4, 0, 0] },
-  { id: 'experience', label: 'Experience', modelName: 'experience', position: [-1.5, 2, -2] },
-  { id: 'projects', label: 'Projects', modelName: 'projects', position: [1.5, -1, -3] },
-  { id: 'contact', label: 'Contact', modelName: 'contact', position: [4, 1, 0] },
+  { id: 'about', label: 'About', modelName: 'about', position: [-6, 0, 0] },
+  { id: 'experience', label: 'Experience', modelName: 'experience', position: [-1, 2, -10] },
+  { id: 'projects', label: 'Projects', modelName: 'projects', position: [1.5, -5, -3] },
+  { id: 'contact', label: 'Contact Me', modelName: 'contact', position: [6, 1, 0] },
 ];
